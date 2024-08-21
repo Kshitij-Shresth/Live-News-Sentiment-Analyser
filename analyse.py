6490c4e7604502e4e2968cbcfe42de717eb1f586
Kshitij Shresth <kshitijshresth@icloud.com>
2024-08-21 14:25:20 +0300

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,1 +1,22 @@
+response = requests.get(endpoint, params=params)
+data = response.json()
+articles = data.get('articles', [])
 
+#Holding sentiment analysis counts in a dictionary
+sentiments = {'positive': 0, 'neutral': 0, 'negative': 0} 
+   for article in articles:
+        title = article.get('title', '')
+        description = article.get('description', '')
+        text = f"{title} {description}"
+     
+#Combining the title and description to form the text for sentiment analysis
+analysis = TextBlob(text)
+        if analysis.sentiment.polarity > 0:
+            sentiments['positive'] += 1
+        elif analysis.sentiment.polarity == 0:
+            sentiments['neutral'] += 1
+        else:
+            sentiments['negative'] += 1
+#Classifying the sentiment based on polarity to update sentiment counts
+          
+return sentiments, articles
